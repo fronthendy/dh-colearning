22514834d0bc4add59a7023b3c5fb162fd612097
fronthendy <hendy.almeida@hotmail.com>
2018-09-26 18:53:58 -0300

    service worker

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Registering ServiceWorker
 if ( 'serviceWorker' in navigator ) {
-    navigator.serviceWorker.register( 'sw.js' ).then(function(registration) {
+    navigator.serviceWorker.register( 'https://fronthendy.github.io/dh-colearning/js/sw.js' ).then(function(registration) {
   
       // Registration was successful
       console.log( 'ServiceWorker registration successful. Scope: ' + registration.scope )
